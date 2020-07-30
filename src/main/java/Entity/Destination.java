564fd55d6e56c6e789e5018bcfcb7f1729db706f
Zimmy Nkqenkqa <zimkhitha.nkqenkqa@gmail.com>
2020-07-31 01:01:32 +0200

    made changes.
    Signed-off-by: Zimmy Nkqenkqa <zimkhitha.nkqenkqa@gmail.com>

---
+++ src/main/java/Entity/Destination.java
@@ -0,0 +1,8 @@
+package Entity;
+
+public class Destination {
+    private String name;
+    private String state;
+    private String country;
+    private int zip_code;
+}
--- src/main/java/Entity/Vehicle.java
+++ src/main/java/Entity/Vehicle.java
@@ -1,5 +1,88 @@
 package Entity;
 
-public class Vehicle {
-    //code
+import java.util.Date;
+
+public class Vehicle extends Destination {
+    private String type;
+    private int vehicle_ID;
+    private double mileage;
+    private String lastServiceDate;
+    private String damage;
+    private String status;
+    private int plate;
+
+    public Vehicle(String typ, int vehicleID, double mile, Date lastServDate, String dmg, String stat, int plat) {
+        //super(zip_code);
+        type = "VolvoTruck";
+        vehicle_ID = 23;
+        mileage = 45380.0;
+        lastServiceDate = "16-03-2020";
+        damage = "Paint cracks";
+        status = "Daily use";
+        plate = 33025923;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getVehicle_ID() {
+        return vehicle_ID;
+    }
+
+    public double getMileage() {
+        return mileage;
+    }
+
+    public String getLastServiceDate() {
+        return lastServiceDate;
+    }
+
+    public String getDamage() {
+        return damage;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public int getPlate() {
+        return plate;
+    }
+
+    public void setType(String typ) {
+        type = typ;
+    }
+
+    public void setVehicle_ID(int vehicleID)
+    {
+        vehicle_ID = vehicleID;
+    }
+
+    public void setMileage(double mile)
+    {
+        mileage = mile;
+    }
+
+    public void setLastServiceDate(String Date)
+    {
+        lastServiceDate = Date;
+    }
+
+    public void setDamage(String dmg)
+    {
+        damage = dmg;
+    }
+
+    public void setStatus(String stat)
+    {
+        status = stat;
+    }
+
+    public void setPlate(int plat)
+    {
+        plate = plat;
+    }
+
 }
+
