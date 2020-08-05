b67adea37796337e6b3ead6c03254b8f3750d576
GrantMetcalf <grantmetcalf13@gmail.com>
2020-08-05 20:41:38 +0200

    Creating of Factory and test classes

---
+++ src/test/java/factory/CourierHistoryFactoryTest.java
@@ -0,0 +1,19 @@
+package factory;
+
+import Entity.CourierHistory;
+
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+
+/*
+public class CourierHistoryFactoryTest {
+
+    @Test
+    public void createCourierHistory() {
+        CourierHistory courierHistory = CourierHistoryFactory.createCourierHistory(char Trip1, int 7708, int 7700, int 007, int 008, int 2, int 123456789, LocalDate 0700, LocalDate 1300, float 1200);
+        System.out.println(courierHistory);
+    }
+}
+*/
