3fc9f0a1f321f70686af7d498fa0038c606db9af
Leaves ZA <yaseenlevy21@gmail.com>
2020-08-25 17:16:11 +0200

    Edited Vehicle and Added VehicleFactory

---
+++ src/main/java/Factory/tool/VehicleFactory.java
@@ -0,0 +1,13 @@
+package Factory.tool;
+import Entity.tool.Vehicle;
+import java.util.Random;
+
+public class VehicleFactory {
+    public static Vehicle createVehicle(String type,double mileage,String lastServiceDate,String damage,String status,int plate){
+        Random rand = new Random();
+        int vehicleID = rand.nextInt(999999999);
+        Vehicle vehicle = new Vehicle.VehicleBuilder().setVehicle_ID(vehicleID).setType(type).setMileage(mileage).setLastServiceDate(lastServiceDate)
+                .setStatus(status).setPlate(plate).createVehicle();
+        return vehicle;
+    }
+}
